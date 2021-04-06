72298adbb98456b935cca8c1bafdf5aa6c5e24c9
kamescg <kamescg@gmail.com>
2021-04-05 21:48:29 -0700

    remove hardhat/consol.so import

--- contracts/Pod.sol
+++ contracts/Pod.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.8.0;
 
-import "hardhat/console.sol";
-
 // Libraries
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
