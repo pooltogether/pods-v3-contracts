fa90a848bb090f1b3fc2b88ac4110b919a488495
kamescg <kamescg@gmail.com>
2021-05-20 13:40:56 -0700

    update deploy mainnet script

--- deploy/mainnet/00_PowerPods.js
+++ deploy/mainnet/00_PowerPods.js
@@ -1,17 +1,32 @@
 const hardhat = require("hardhat");
-const { getConfig } = require("../../lib/config");
 const { getPodAndDropAddress } = require("../../lib/deploy");
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   provider = hardhat.ethers.provider;
   const { execute } = deployments;
-  const { deployer } = await getNamedAccounts();
+
+  const {
+    deployer,
+    prizePoolDAI,
+    prizePoolDAITicket,
+    prizePoolDAIFaucet,
+    prizePoolUSDC,
+    prizePoolUSDCTicket,
+    prizePoolUSDCFaucet,
+    prizePoolCOMP,
+    prizePoolCOMPTicket,
+    prizePoolCOMPFaucet,
+    prizePoolUNI,
+    prizePoolUNITicket,
+    prizePoolUNIFaucet,
+    prizePoolPOOL,
+    prizePoolPOOLTicket,
+    prizePoolPOOLFaucet,
+  } = await getNamedAccounts();
   const chainId = await getChainId();
 
   // Check ChainID (Mainnet or MainnetFork)
   if (chainId == 1 || chainId == 1337) {
-    const CONFIG = getConfig("mainnet");
-
     // Create DAI Pod
     const createDAI = await execute(
       "PodFactory",
@@ -19,14 +34,12 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
         from: deployer,
       },
       "create",
-      CONFIG.podDAI.prizePool,
-      CONFIG.podDAI.ticket,
-      CONFIG.podDAI.faucet,
+      prizePoolDAI,
+      prizePoolDAITicket,
+      prizePoolDAIFaucet,
       deployer,
       18
     );
-
-    await getPodAndDropAddress(createDAI.transactionHash, deployments, "DAI");
 
     const createUSDC = await execute(
       "PodFactory",
@@ -34,14 +47,12 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
         from: deployer,
       },
       "create",
-      CONFIG.podUSDC.prizePool,
-      CONFIG.podUSDC.ticket,
-      CONFIG.podUSDC.faucet,
+      prizePoolUSDC,
+      prizePoolUSDCTicket,
+      prizePoolUSDCFaucet,
       deployer,
-      18
+      6
     );
-
-    await getPodAndDropAddress(createUSDC.transactionHash, deployments, "USDC");
 
     const createUNI = await execute(
       "PodFactory",
@@ -49,14 +60,12 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
         from: deployer,
       },
       "create",
-      CONFIG.podUNI.prizePool,
-      CONFIG.podUNI.ticket,
-      CONFIG.podUNI.faucet,
+      prizePoolUNI,
+      prizePoolUNITicket,
+      prizePoolUNIFaucet,
       deployer,
-      6
+      18
     );
-
-    await getPodAndDropAddress(createUNI.transactionHash, deployments, "UNI");
 
     const createCOMP = await execute(
       "PodFactory",
@@ -64,15 +73,32 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
         from: deployer,
       },
       "create",
-      CONFIG.podCOMP.prizePool,
-      CONFIG.podCOMP.ticket,
-      CONFIG.podCOMP.faucet,
+      prizePoolCOMP,
+      prizePoolCOMPTicket,
+      prizePoolCOMPFaucet,
       deployer,
       18
     );
 
+    const createPOOL = await execute(
+      "PodFactory",
+      {
+        from: deployer,
+      },
+      "create",
+      prizePoolPOOL,
+      prizePoolPOOLTicket,
+      prizePoolPOOLFaucet,
+      deployer,
+      18
+    );
+
+    await getPodAndDropAddress(createDAI.transactionHash, deployments, "DAI");
+    await getPodAndDropAddress(createUSDC.transactionHash, deployments, "USDC");
+    await getPodAndDropAddress(createUNI.transactionHash, deployments, "UNI");
     await getPodAndDropAddress(createCOMP.transactionHash, deployments, "COMP");
+    await getPodAndDropAddress(createPOOL.transactionHash, deployments, "POOL");
   }
 };
 
-module.exports.tags = ["Factories"];
+module.exports.tags = ["Pods"];
